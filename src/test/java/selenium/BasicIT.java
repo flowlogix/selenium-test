aee2e3fe8a6aa5e0a8c58707290e8df1472520cb
lprimak <lenny@flowlogix.com>
2024-01-10 23:49:17 -0600

    dynamic configuration

--- src/test/java/selenium/BasicIT.java
+++ src/test/java/selenium/BasicIT.java
@@ -15,19 +15,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.BuildInfo;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.grid.Main;
 import org.openqa.selenium.manager.SeleniumManager;
+import org.openqa.selenium.remote.AbstractDriverOptions;
 import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -36,6 +40,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -71,19 +76,28 @@ public class BasicIT {
 
     @BeforeAll
     static void setupAll() {
-//        downloadManager();
-        startGrid();
+        if (System.getProperty("selenium.manager") != null) {
+            downloadManager();
+        }
+        if (System.getProperty("selenium.grid") != null) {
+            startGrid();
+        }
         var startTime1 = Instant.now();
-        ChromeDriverService chromeDriverService = new ChromeDriverService.Builder()
+        ChromeDriverService chromeDriverService =
+                switch (System.getProperty("selenium.browser", "chrome").toLowerCase()) {
+                    case "chrome" -> new ChromeDriverService.Builder()
 //                .withVerbose(true)
-                .withLogOutput(System.out)
-                .build();
+                            .withLogOutput(System.out)
+                            .build();
+                    default -> null;
+                };
+
         ChromeOptions chromeOptions = new ChromeOptions();
 //        chromeOptions.setBinary("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
         chromeOptions.addArguments("--headless=new");
 
         FirefoxOptions firefoxOptions = new FirefoxOptions();
-        firefoxOptions.addArguments("-headless");
+        firefoxOptions.addArguments("--headless");
 
         SafariOptions safariOptions = new SafariOptions();
 
@@ -93,9 +107,33 @@ public class BasicIT {
         System.out.format("setupSession() took %02d.%04d seconds \n", duration1.toSeconds(), duration1.toMillis());
 
         var startTime2 = Instant.now();
-//        driver = new FirefoxDriver(firefoxOptions);
-//        driver = new ChromeDriver(chromeDriverService, chromeOptions);
-        driver = new RemoteWebDriver(chromeOptions);
+
+        record Driver(Supplier<WebDriver> driver, Capabilities options) {
+            Driver(Supplier<WebDriver> driver, Capabilities options) {
+                this.driver = driver;
+                this.options = options;
+            }
+        }
+
+        Driver initDriver = switch (System.getProperty("selenium.browser", "chrome")) {
+            case "chrome" -> new Driver(() -> new ChromeDriver(chromeDriverService, chromeOptions), chromeOptions);
+            case "firefox" -> new Driver(() -> new FirefoxDriver(firefoxOptions), firefoxOptions);
+            case "edge" -> new Driver(() -> new EdgeDriver(edgeOptions), edgeOptions);
+            case "safari" -> new Driver(() -> new SafariDriver(safariOptions), safariOptions);
+            default -> throw new IllegalStateException();
+        };
+
+        String browserVersion = System.getProperty("selenium.version");
+        if (browserVersion != null) {
+            var caps = (AbstractDriverOptions<?>) initDriver.options();
+            caps.setBrowserVersion(browserVersion);
+        }
+
+        if (System.getProperty("selenium.grid") != null) {
+            driver = new RemoteWebDriver(initDriver.options());
+        } else {
+            driver = initDriver.driver.get();
+        }
         driver = new Augmenter().augment(driver);
 
         var endTime2 = Instant.now();
